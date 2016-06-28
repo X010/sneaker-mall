149e8546fa98ac8b8bdf51eb288a2bbf620484d1
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-06-28 22:48:55 +0800

    add show start

--- Admin/Admin/Controller/CouponController.class.php
+++ Admin/Admin/Controller/CouponController.class.php
@@ -7,7 +7,7 @@ class CouponController extends BaseController
 {
 
     private $r = array("error" => 0, "msg" => "", "data" => "");
-    private $limit = 15;
+    private $limit = 13;
 
     public function index()
     {
@@ -19,17 +19,17 @@ class CouponController extends BaseController
      */
     public function clist()
     {
-        $search_key = I('key','');
-        $p = I('p',0,'intval');
+        $search_key = I('key', '');
+        $p = I('p', 0, 'intval');
         $company = $this->get_company();
 
         $res = $this->getCouponList($company['id'], $p);
         $count = $this->getCouponCount($company['id']);
 
-        $this->assign('coupon_list',$res);
+        $this->assign('coupon_list', $res);
         $Page = new \Extend\Page($count, $this->limit);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show = $Page->show();// 分页显示输出
-        $this->assign('page',$show);
+        $this->assign('page', $show);
         $this->display('Coupon:clist');
     }
 
@@ -59,18 +59,19 @@ class CouponController extends BaseController
         $data['coupon_use_end'] = I('coupon_use_end', '');
         $data['company_id'] = $company['id'];
         $data['coupon_status'] = 2;
-
-        $id = $model->add($data);
+        if (!empty($data['coupon_name']) && $data['coupon_money'] > 0) {
+            $id = $model->add($data);
+        }
 
         //绑定数据并返回
         $res = $this->getCouponList($company['id'], 0);
         $count = $this->getCouponCount($company['id']);
 
 
-        $this->assign('coupon_list',$res);
+        $this->assign('coupon_list', $res);
         $Page = new \Extend\Page($count, $this->limit);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show = $Page->show();// 分页显示输出
-        $this->assign('page',$show);
+        $this->assign('page', $show);
         $this->display('Coupon:clist');
     }
 
@@ -84,12 +85,13 @@ class CouponController extends BaseController
         $where = " company_id=$cid";
 
         $start = 0;
-        if ($p >= 1) {
+        if ($p) {
             $start = ($p - 1) * $this->limit;
         } else {
             $start = 0;
         }
 
+        var_dump($start);
         $res = $model->where($where)->limit($start, $this->limit)->order('coupon_send_start DESC')->select();
         return $res;
     }
