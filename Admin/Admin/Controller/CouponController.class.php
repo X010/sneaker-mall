6d0d9be330fb336f81a590dfbd907122acf5abd1
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-06-28 20:21:58 +0800

    check num string

--- Admin/Admin/Controller/CouponController.class.php
+++ Admin/Admin/Controller/CouponController.class.php
@@ -35,6 +35,19 @@ class CouponController extends BaseController
      */
     public function save()
     {
+        $model = M('coupon');
+        $company = $this->get_company();
+        $data['coupon_name'] = I('coupon_name', '');
+        $data['coupon_money'] = I('coupon_money', '');
+        $data['coupon_small_money'] = I('coupon_small_money', '');
+        $data['coupon_type'] = I('coupon_type', '');
+        $data['coupon_send_start'] = I('coupon_send_start', '');
+        $data['coupon_send_end'] = I('coupon_send_end', '');
+        $data['coupon_use_start'] = I('coupon_use_start', '');
+        $data['coupon_use_end'] = I('coupon_use_end', '');
+        $data['company_id'] = $company['id'];
+        $data['coupon_status'] = 2;
+        $id = $model->add($data);
 
         $this->display('Coupon:clist');
     }
